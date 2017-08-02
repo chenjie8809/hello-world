792b543ae4f02399eaac922945656a4a7ff22065
chenjie8809 <chenjie8809@163.com>
2017-08-02 09:42:47 +0800

    first commit.

---
+++ src/com/kedong/interpss/helloworld.java
@@ -0,0 +1,32 @@
+/* 
+ * Java file created by Chenjie
+ *
+ * Created on 2017年8月2日
+ * Copyright 2017 Beijing Kedong Electrical Control System Ltd. Co.
+ * All rights reserved.
+ */
+/**
+ * 
+ */
+package com.kedong.interpss;
+
+/**
+ * Chejie's Type
+ *
+ * @author cj
+ * @version 1.0, 2017年8月2日
+ * Copyright 2017 Beijing Kedong ELectrical Control System Ltd. Co.
+ * All rights reserved.
+ */
+public class helloworld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World！");
+
+	}
+
+}
